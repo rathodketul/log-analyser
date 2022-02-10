b9ec861ec0925e80b20b19f80847a8497569e20d
Loyalty <yogesh.baraiya@futuregroup.in>
2022-02-10 20:23:32 +0530

    some changes

--- src/main/java/com/ketul/loganalyser/model/Event.java
+++ src/main/java/com/ketul/loganalyser/model/Event.java
@@ -1,9 +1,13 @@
 package com.ketul.loganalyser.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
+@Data
 public class Event {
     @JsonProperty("id")
     private String id;
@@ -19,46 +23,6 @@ public class Event {
 
     @JsonProperty("timestamp")
     private long timestamp;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public State getState() {
-        return state;
-    }
-
-    public void setState(State state) {
-        this.state = state;
-    }
-
-    public EventType getType() {
-        return type;
-    }
-
-    public void setType(EventType type) {
-        this.type = type;
-    }
-
-    public String getHost() {
-        return host;
-    }
-
-    public void setHost(String host) {
-        this.host = host;
-    }
-
-    public long getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(long timestamp) {
-        this.timestamp = timestamp;
-    }
 
     @Override
     public String toString() {
--- src/main/java/com/ketul/loganalyser/validator/LogAnalyserValidator.java
+++ src/main/java/com/ketul/loganalyser/validator/LogAnalyserValidator.java
@@ -1,51 +1,51 @@
 package com.ketul.loganalyser.validator;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 import com.ketul.loganalyser.model.Context;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 public class LogAnalyserValidator {
-    private static final Logger LOGGER = LoggerFactory.getLogger(LogAnalyserValidator.class);
 
-    public void validateInput(Context context, String... args) {
-        LOGGER.info("Validating the input...");
+	public void validateInput(Context context, String... args) {
+		log.info("Validating the input...");
 
-        validateArguments(args);
-        validateFilePath(context, args[1]);
-    }
+		validateArguments(args);
+		validateFilePath(context, args[1]);
+	}
 
-    private void validateFilePath(Context context, String logFilePath) {
-        LOGGER.info("Log file specified for LogAnalyserService: {}", logFilePath);
-        context.setLogFilePath(logFilePath);
+	private void validateFilePath(Context context, String logFilePath) {
+		log.info("Log file specified for LogAnalyserService: {}", logFilePath);
+		context.setLogFilePath(logFilePath);
 
-        try {
-            File file = new ClassPathResource("samples/" + logFilePath).getFile();
-            if (!file.exists()) {
-                file = new ClassPathResource(logFilePath).getFile();
-                if (!file.exists()) {
-                    file = new File(logFilePath);
-                }
-            }
+		try {
+			File file = new ClassPathResource("samples/" + logFilePath).getFile();
+			if (!file.exists()) {
+				file = new ClassPathResource(logFilePath).getFile();
+				if (!file.exists()) {
+					file = new File(logFilePath);
+				}
+			}
 
-            if (!file.exists())
-                throw new FileNotFoundException("Unable to open the file " + logFilePath);
-        } catch (IOException e) {
-            LOGGER.error("!!! Unable to find the specified file '{}'", logFilePath);
-        }
-    }
+			if (!file.exists())
+				throw new FileNotFoundException("Unable to open the file " + logFilePath);
+		} catch (IOException e) {
+			log.error("!!! Unable to find the specified file '{}'", logFilePath);
+		}
+	}
 
-    private void validateArguments(String[] args) {
-        LOGGER.debug("Validating the program arguments...");
-        if (args.length < 1) {
-            throw new IllegalArgumentException("!!! Please specify the filepath to analyse.");
-        }
-    }
+	private void validateArguments(String[] args) {
+		log.debug("Validating the program arguments...");
+		if (args.length < 1) {
+			throw new IllegalArgumentException("!!! Please specify the filepath to analyse.");
+		}
+	}
 }
